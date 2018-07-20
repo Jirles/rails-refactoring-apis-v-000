c27b7d5e7d0aeb2fb0507834e52b48ef9ac6018a
Emily Jirles <emily.jirles@gmail.com>
2018-07-20 16:21:40 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,13 +5,8 @@ class RepositoriesController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
     github = GithubService.new(session[:service])
     github.create_repo(params[:name])
     redirect_to root_path
-=======
-    github = GithubService.new(session{:service})
-    github.create_repo(params[:name])
->>>>>>> dda808de02288c8a1bc201f921b68ee5316c0384
   end
 end
