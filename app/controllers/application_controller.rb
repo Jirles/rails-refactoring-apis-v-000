16964b46a07ff449458befb0f1d65370a80a332a
Emily Jirles <emily.jirles@gmail.com>
2018-07-20 16:21:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,7 @@ class ApplicationController < ActionController::Base
   private
 
     def authenticate_user
-<<<<<<< HEAD
       redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&redirect_uri=#{CGI.escape(auth_url)}&scope=repo" if !logged_in?
-=======
-      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&redirect_uri=#{CGI.escape(auth_path)}&scope=repo" if !logged_in?
->>>>>>> dda808de02288c8a1bc201f921b68ee5316c0384
     end
 
     def logged_in?
